421e373b65a8003cc6cf90841e594de0da621ab9
Andrika Balint <andrika.balint@gmail.com>
2017-09-24 21:59:14 +0200

    Moving Game Objects with kinect test

---
+++ Kinect test 3/Assets/DetectJoints.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class DetectJoints : MonoBehaviour {
+
+    public GameObject BodySrcManager;
+    public JointType TrackedJoint;
+    private BodySourceManager bodyManager;
+    private Body[] bodies;
+    public float speed = 10f;
+	void Start () {
+        if (BodySrcManager == null)
+        {
+            Debug.Log("Assign body manager");
+
+        }
+        else
+        {
+            bodyManager = BodySrcManager.GetComponent<BodySourceManager>();
+        }
+	}
+	
+	
+	void Update () {
+        if (bodyManager == null)
+        {
+            return;
+        }
+        bodies = bodyManager.GetData();
+        if (bodies == null)
+        {
+            return;
+        }
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                var pos = body.Joints[TrackedJoint].Position;
+                gameObject.transform.position = new Vector3(pos.X * speed, pos.Y * speed, pos.Z * speed);
+            }
+        }
+
+	}
+}
