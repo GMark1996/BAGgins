1b4d22795c178b5ca9960dfb3b97e0ea638441fb
lvbubi <lvbubi2@gmail.com>
2017-10-14 22:13:46 +0200

    Init Video Player #6

---
+++ Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Microsoft.Samples.Kinect.DiscreteGestureBasics
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        FileInfo[] Files = null;
+        //string recognisedMovement = "edsh";
+        public Window1()
+        {
+            InitializeComponent();
+            DirectoryInfo d = new DirectoryInfo(@"C:\LOTR");
+            Files = d.GetFiles("*.mp4");
+
+            //playVideo(recognisedMovement);
+        }
+
+        public void playVideo(String movement)
+        {
+            foreach (FileInfo f in Files)
+            {
+                if (f.Name.Split('.').First() == movement)
+                    VideoControl.Source = new Uri(f.FullName);
+            }
+        }
+    }
+}
