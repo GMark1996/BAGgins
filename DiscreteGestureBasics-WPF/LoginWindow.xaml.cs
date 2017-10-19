43fcf29b72fbe726aa70ea9d9a7483f744adc421
GMark1996 <gyjned@student.mik.uni-pannon.hu>
2017-10-20 01:54:37 +0200

    formating and keyboard implemented and kinectmousecontrol installed

--- DiscreteGestureBasics-WPF/LoginWindow.xaml.cs
+++ DiscreteGestureBasics-WPF/LoginWindow.xaml.cs
@@ -20,14 +20,33 @@ namespace Microsoft.Samples.Kinect.DiscreteGestureBasics
     /// </summary>
     public partial class LoginWindow : Page
     {
+        List<Button> buttons = new List<Button>();
         List<UserScore> Users = new List<UserScore>();
+        Boolean useUpperCase = false;
+        Boolean useCapsLock = false;
 
         public LoginWindow()
         {
             InitializeComponent();
 
+            DiscreteGestureBasicsWPF.KinectMainWindow kinectMainWindow = new DiscreteGestureBasicsWPF.KinectMainWindow();
             ScoreBoard scoreboard = new ScoreBoard();
             Users = scoreboard.getUserScores();
+
+            buttons.Add(ö); buttons.Add(ü); buttons.Add(ó);
+            buttons.Add(q); buttons.Add(w); buttons.Add(e);
+            buttons.Add(r); buttons.Add(t); buttons.Add(z);
+            buttons.Add(u); buttons.Add(i); buttons.Add(o);
+            buttons.Add(p); buttons.Add(ő); buttons.Add(ú);
+            buttons.Add(a); buttons.Add(s); buttons.Add(d);
+            buttons.Add(f); buttons.Add(g); buttons.Add(h);
+            buttons.Add(j); buttons.Add(k); buttons.Add(l);
+            buttons.Add(é); buttons.Add(á); buttons.Add(ű);
+            buttons.Add(í); buttons.Add(y); buttons.Add(x);
+            buttons.Add(c); buttons.Add(v); buttons.Add(b);
+            buttons.Add(n); buttons.Add(m);
+            SetKeyBoardToUpperCase();
+            useUpperCase = true;
         }
 
         private void Login(object sender, RoutedEventArgs e)
@@ -35,6 +54,72 @@ namespace Microsoft.Samples.Kinect.DiscreteGestureBasics
                UserScore user = new UserScore(Name.Text);
                NavigationService ns = NavigationService.GetNavigationService(this);
                ns.Navigate(new Uri("MainMenu.xaml", UriKind.Relative));
+        }
+
+        private void KeyBoard(object sender, RoutedEventArgs e)
+        {
+            var keyword = (e.Source as Button).Content.ToString();
+            Name.Text += keyword;
+            if (useCapsLock)
+            {
+                SetKeyBoardToUpperCase();
+            }
+            if (useUpperCase)
+            {
+                SetKeyBoardToLowerCase();
+                useUpperCase = false;
+            }
+        }
+
+        private void SetKeyBoardToUpperCase()
+        {
+            foreach (var button in buttons)
+            {
+                var stri = button.Content as string;
+                button.Content = stri.ToUpper();
+            }
+        }
+
+        private void SetKeyBoardToLowerCase()
+        {
+            foreach (var button in buttons)
+            {
+                var stri = button.Content as string;
+                button.Content = stri.ToLower();
+            }
+        }
+
+        private void ShiftPress(object sender, RoutedEventArgs e)
+        {
+            if (useUpperCase || useCapsLock)
+            {
+                SetKeyBoardToLowerCase();
+                useUpperCase = false;
+            }
+            else
+            {
+                SetKeyBoardToUpperCase();
+                useUpperCase = true;
+            }
+        }
+
+        private void CapsLockButton(object sender, RoutedEventArgs e)
+        {
+            if (useCapsLock)
+            {
+                SetKeyBoardToLowerCase();
+                useCapsLock = false;
+            }
+            else
+            {
+                SetKeyBoardToUpperCase();
+                useCapsLock = true;
+            }
+        }
+
+        private void DeleteButton(object sender, RoutedEventArgs e)
+        {
+            Name.Text = Name.Text.Remove(Name.Text.Length - 1);
         }
     }
 }
--- DiscreteGestureBasics-WPF/Properties/Settings.Designer.cs
+++ DiscreteGestureBasics-WPF/Properties/Settings.Designer.cs
@@ -22,5 +22,94 @@ namespace Microsoft.Samples.Kinect.GestureBasics.Properties {
                 return defaultInstance;
             }
         }
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.2")]
+        public float CursorSmoothing
+        {
+            get
+            {
+                return ((float)(this["CursorSmoothing"]));
+            }
+            set
+            {
+                this["CursorSmoothing"] = value;
+            }
+        }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool GripGesture
+        {
+            get
+            {
+                return ((bool)(this["GripGesture"]));
+            }
+            set
+            {
+                this["GripGesture"] = value;
+            }
+        }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("3.5")]
+        public float MouseSensitivity
+        {
+            get
+            {
+                return ((float)(this["MouseSensitivity"]));
+            }
+            set
+            {
+                this["MouseSensitivity"] = value;
+            }
+        }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool DoClick
+        {
+            get
+            {
+                return ((bool)(this["DoClick"]));
+            }
+            set
+            {
+                this["DoClick"] = value;
+            }
+        }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("60")]
+        public float PauseThresold
+        {
+            get
+            {
+                return ((float)(this["PauseThresold"]));
+            }
+            set
+            {
+                this["PauseThresold"] = value;
+            }
+        }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        public float PauseToClickTime
+        {
+            get
+            {
+                return ((float)(this["PauseToClickTime"]));
+            }
+            set
+            {
+                this["PauseToClickTime"] = value;
+            }
+        }
     }
 }
